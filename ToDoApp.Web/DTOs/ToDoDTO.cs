dd615312fa37c5151d0e1ac6a113602523ab890d
TuAFBogey <uguvenen@gmail.com>
2021-11-23 21:25:47 +0300

    DTO ve mapping eklendi.

---
+++ ToDoApp.Web/DTOs/ToDoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Core.Enums;
+
+namespace ToDoApp.Web.DTOs
+{
+    public class ToDoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool IsComplete { get; set; }
+        public Period Period { get; set; }
+    }
+}
---
+++ ToDoApp.Web/Mapping/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Core.Enums;
+using ToDoApp.Core.Models;
+using ToDoApp.Web.DTOs;
+
+namespace ToDoApp.Web.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<ToDo, ToDoDTO>();
+            CreateMap<ToDoDTO, ToDo>();
+        }
+    }
+}
